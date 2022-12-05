d2b9165daa9dd1ce474d9a5e61303cd001f21252
Kayla Lewis <caela.calculensis@gmail.com>
2022-12-04 19:45:25 -0500

    committing

---
+++ pythonCode/topic_analysis/clean_tweets.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import re
+
+tweets_df = pd.read_csv("my_tweets.csv")
+
+#remove all columns except those containing tweets
+tweets_df = tweets_df.drop(columns=['Date Created', \
+            'Number of Likes', 'Source of Tweet'],  \
+            axis=1).sample(4000)
+
+# remove punctuation
+tweets_df['Tweets'] = \
+    tweets_df['Tweets'].map(lambda x: \
+    re.sub('[,\.!?]','', x))
+
+# remove all the mentions (@)
+tweets_df['Tweets'] = \
+    tweets_df['Tweets'].map(lambda x: \
+    re.sub('@([a-zA-Z0-9_]{1,50})', '', x))
+
+# convert the titles to lowercase
+tweets_df['Tweets'] = \
+    tweets_df['Tweets'].map(lambda x: x.lower())
+
+tweets_df.to_csv("my_tweets_cleaned.csv")
--- pythonCode/topic_analysis/tweetScrape.py
+++ pythonCode/topic_analysis/tweetScrape.py
@@ -6,9 +6,11 @@ attributes_container = []
 
 # Using TwitterSearchScraper to scrape data and append tweets to list
 for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:Estimatrix').get_items()):
-    if i>3000:
+    if i>4000:
         break
     attributes_container.append([tweet.date, tweet.likeCount, tweet.sourceLabel, tweet.content])
     
 # Creating a dataframe from the tweets list above 
 tweets_df = pd.DataFrame(attributes_container, columns=["Date Created", "Number of Likes", "Source of Tweet", "Tweets"])
+
+tweets_df.to_csv("my_tweets.csv")
